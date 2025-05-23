4aee8b39da6387799dfb43e22eb9b774da105f1b
dlanx6 <144339211+dlanx6@users.noreply.github.com>
2025-05-23 15:40:53 +0800

    add: smart contract instance function and data

---
+++ utils/getSmartContract.ts
@@ -0,0 +1,15 @@
+import { BrowserProvider, JsonRpcSigner, Contract } from "ethers";
+import { contractABI, contractAddress } from "./contractDetails";
+
+export const getSmartContract = async (): Promise<Contract> => {
+	const ethProvider: BrowserProvider = new BrowserProvider(window.ethereum);
+	const ethSigner: JsonRpcSigner = await ethProvider.getSigner();
+
+	const contract: Contract = new Contract(
+		contractAddress,
+		contractABI,
+		ethSigner
+	);
+
+	return contract;
+};
