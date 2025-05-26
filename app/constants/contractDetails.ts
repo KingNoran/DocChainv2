d5136b2941316488e4f008ad787ba12cc5120334
dlanx6 <144339211+dlanx6@users.noreply.github.com>
2025-05-26 15:05:07 +0800

    redeploy

---
+++ app/constants/contractDetails.ts
@@ -0,0 +1,5 @@
+import abi from "./Tokenizer.json";
+import { InterfaceAbi } from "ethers";
+
+export const contractAddress: string = "0x37D18Cc18404d27D096AcD70D518c2a74cBC85d2";
+export const contractABI: InterfaceAbi = abi.abi;
